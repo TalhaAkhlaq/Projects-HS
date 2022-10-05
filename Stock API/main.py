e1d5ae4134441c3eb987a35f4c21144a996d16be
TalhaAkhlaq <talhaaklaq1@gmail.com>
2022-10-04 20:05:00 -0400

    Stock API

--- Stock API/main.py
+++ Stock API/main.py
@@ -1,8 +1,16 @@
 import requests
 import time
 
+# API Key
+# https://twelvedata.com/
+# https://twelvedata.com/pricing
+
 ticker = "MSFT"
 api_key = "4e61eafd710346478686d7eda5415321"
+
+# Main
+# print(get_stock_price(ticker, api_key))
+# print(get_stock_quote(ticker, api_key))
 
 def get_stock_price(ticker_symbol, api):
     url = f"https://api.twelvedata.com/price?symbol={ticker_symbol}&apikey={api}"
@@ -10,11 +18,22 @@ def get_stock_price(ticker_symbol, api):
     price = response['price'][:-3]
     return price
 
+# Path: Stock API\main.py
+# Compare this snippet from main.py:
+
 
 def get_stock_quote(ticker_symbol, api):
     url = f"https://api.twelvedata.com/quote?symbol={ticker_symbol}&apikey={api}"
     response = requests.get(url).json()
     return response
+
+
+# Path: Stock API\main.py
+# Compare this snippet from main.py:
+# if hands:
+# for hand in hands:
+# drawing_utils.draw_landmarks(frame, hand)
+# for id, lm in enumerate(hand.landmark):
 
 stockdata = get_stock_quote(ticker, api_key)
 stock_price = get_stock_price(ticker, api_key)
